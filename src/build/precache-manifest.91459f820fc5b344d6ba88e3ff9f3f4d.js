cae1ad099dfd607d990c18356ecfdd91d2726e01
xxxreed <wxie3430@usc.edu>
2020-04-21 21:17:59 -0700

    final commit

---
+++ src/build/precache-manifest.91459f820fc5b344d6ba88e3ff9f3f4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4f3f892f6eed53826baef13e77e26f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e73e124d1840a6876c41",
+    "url": "/static/css/2.db20d504.chunk.css"
+  },
+  {
+    "revision": "5cb3949bcafc37d14dc6",
+    "url": "/static/css/main.d086810a.chunk.css"
+  },
+  {
+    "revision": "e73e124d1840a6876c41",
+    "url": "/static/js/2.43b57739.chunk.js"
+  },
+  {
+    "revision": "5545cb2ced5a80c45120bcb0d570db6a",
+    "url": "/static/js/2.43b57739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb3949bcafc37d14dc6",
+    "url": "/static/js/main.7bd51031.chunk.js"
+  },
+  {
+    "revision": "28e77d4281d3f90b5194",
+    "url": "/static/js/runtime-main.3a36f248.js"
+  }
+]);
